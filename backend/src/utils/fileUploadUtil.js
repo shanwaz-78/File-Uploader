f73730c7eab97df3c284b08dfe200d2a00d14af3
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2025-03-27 01:11:19 +0530

    initial commit

--- backend/src/utils/fileUploadUtil.js
+++ backend/src/utils/fileUploadUtil.js
@@ -4,10 +4,11 @@ export const uploadFile = async (fileBuffer, mimeType, folder = "uploads") => {
   try {
     const b64 = Buffer.from(fileBuffer).toString("base64");
     const dataURI = `data:${mimeType};base64,${b64}`;
-    const result = cloudinary.uploader.upload(dataURI, {
+    const result = await cloudinary.uploader.upload(dataURI, {
       resource_type: "auto",
       folder,
     });
+    console.log(result)
     return {
       message: "File processed successfully.",
       url: result.secure,
